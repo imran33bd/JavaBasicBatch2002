8cd73dd13067f38321c93a44ec5ded1eb8ec5d50
ren6728 <hreng6728@gmail.com>
2020-08-12 21:45:55 -0400

    class17,inheritance,overide,overloading,javaio

---
+++ src/Inheritance/SomeOtherPhone.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class SomeOtherPhone {
+    final int x=5;
+    public void method(){
+       // x=10;
+        System.out.println("other method...");
+    }
+}
---
+++ src/Javaio/FileReadingDemo.java
@@ -0,0 +1,17 @@
+package Javaio;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingDemo{
+    public void readFile() throws IOException {
+        String filePath ="/Users/renxing/Izzan/IdeaProjects/JavaBasicBatch2002/Data/Demo.txt";
+        FileReader fileReader=new FileReader(filePath);
+        // fileReader.read()
+        int i;
+        while ((i=fileReader.read()) != -1){
+            System.out.print((char)i);
+        }
+    }
+}
